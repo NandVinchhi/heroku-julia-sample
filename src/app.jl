63a736458735d2d45d5019b691d7c7721e6e2c0b
NandVinchhi <58524154+NandVinchhi@users.noreply.github.com>
2019-12-19 22:03:15 +0530

    Update app.jl

--- src/app.jl
+++ src/app.jl
@@ -1,40 +1,12 @@
 using Mux
-using Dashboards
-import HTTP
 
-external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
-
-@app test = Dash("Plastic Exports", external_stylesheets = external_stylesheets) do
-   
-       
-        Mux.defaults,
-        page(dcc_graph(
-            id = "my-graph",
-
-            figure = (
-                data = [
-                (x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
-                y=[219, 146, 112, 127, 124, 180, 236, 207, 236, 263, 350, 430, 474, 526, 488, 537, 500, 439],
-                name = "Rest of world"),
-
-                (x = [1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
-                y = [16, 13, 10, 11, 28, 37, 43, 55, 56, 88, 105, 156, 270, 299, 340, 403, 549, 499],
-                name = "China"),
-
-                ],
-
-                layout = (
-                    title = "US export of plastic scrap",
-                    showlegend = true,
-                    legend = (x = 0, y = 1.0),
-                    margin = (l = 40, r = 0, t = 40, b = 30)
-                ),
-
-               
-
-            )
-        ))
-    
-end
+@app test = (
+  Mux.defaults,
+  page(respond("<h1>Hello JuliaCon!</h1>")),
+  page("/about",
+       probabilty(0.1, respond("<h1>Boo!</h1>")),
+       respond("<h1>About Me</h1>")),
+  page("/user/:user", req -> "<h1>Hello, $(req[:params][:user])!</h1>"),
+  Mux.notfound())
 
 fetch(serve(test,parse(Int,ARGS[1])))
